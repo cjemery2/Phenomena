f086b2d1f466dc8007fd47e433b0b93390f8923d
cjemery <cjemery2@hotmail.com>
2021-10-23 12:19:49 -0400

    project done

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 // Build an apiRouter using express Router
-
+const express = require('express');
+const apiRouter = express.Router();
 
 // Import the database adapter functions from the db
-
+const {
+    client,
+    createReport,
+    getOpenReports,
+    _getReport,
+    closeReport,
+    createReportComment,
+  } = require('../db/index');
 
 /**
  * Set up a GET request for /reports
@@ -12,6 +20,25 @@
  * - on success, it should send back an object like { reports: theReports }
  * - on caught error, call next(error)
  */
+ apiRouter.get("/reports", async (req, res, next) => {
+    try{
+    const openReports = await getOpenReports();
+    let obj = {reports: [] }
+    if(openReports){
+        obj.reports = openReports;
+        res.send(obj);
+      } else{
+        next({
+          name: 'error',
+          message: 'openReports'
+        })
+      }
+  
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
+
 
 
 
@@ -23,6 +50,23 @@
  * - on success, it should send back the object returned by createReport
  * - on caught error, call next(error)
  */
+ apiRouter.post("/reports", async (req, res, next) => {
+    const { title, location, description, password } = req.body;
+    try{
+        const report = await createReport(req.body);
+        if(report){
+            res.send(report);
+          } else{
+            next({
+              name: 'error',
+              message: 'createReport'
+            })
+          }
+      
+        } catch ({ name, message }) {
+          next({ name, message });
+        }
+});
 
 
 
@@ -35,7 +79,23 @@
  * - on success, it should send back the object returned by closeReport
  * - on caught error, call next(error)
  */
-
+ apiRouter.delete("/reports/:reportId", async (req, res, next) => {
+     
+    try{
+        const close = await closeReport(req.params.reportId, req.body.password);
+        if(close){
+            res.send(close);
+          } else{
+            next({
+              name: 'error',
+              message: 'closeReport'
+            })
+          }
+      
+        } catch ({ name, message }) {
+          next({ name, message });
+        }
+});
 
 
 /**
@@ -47,7 +107,25 @@
  * - on success, it should send back the object returned by createReportComment
  * - on caught error, call next(error)
  */
+ apiRouter.post("/reports/:reportId/comments", async (req, res, next) => {
+    const { title, location, description, password } = req.body;
+    try{
+        const close = await createReportComment(req.params.reportId, req.body);
+        if(close){
+            res.send(close);
+          } else{
+            next({
+              name: 'error',
+              message: 'closeReport'
+            })
+          }
+      
+        } catch ({ name, message }) {
+          next({ name, message });
+        }
+});
 
 
 
 // Export the apiRouter
+module.exports = apiRouter;
--- index.js
+++ index.js
@@ -25,3 +25,45 @@
 
 // Start the server listening on port PORT
 // On success, connect to the database
+require('dotenv').config();
+const { PORT = 3000 } = process.env
+const express = require('express');
+const server = express();
+server.use(express.json())
+
+const morgan = require('morgan');
+const { application_name } = require('pg/lib/defaults');
+server.use(morgan('dev'));
+
+
+// Import cors 
+// Have the server use cors()
+const cors = require('cors');
+server.use(cors());
+
+// Have the server use bodyParser.json()
+const bodyParser = require('body-parser')
+
+server.use(bodyParser.json());
+// Have the server use your api router with prefix '/api'
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+// Import the client from your db/index.js
+const { client } = require("./db");
+// Create custom 404 handler that sets the status code to 404.
+server.use( (req, res, next)=> {
+    res.status(404).send("Status set to 404")
+  })
+// Create custom error handling that sets the status code to 500
+// and returns the error as an object
+server.use((req, res, next)=> {
+   try{
+       res.status(500).send("Status set to 500")
+   } catch (error){
+       return {message: "Trouble setting status to 500"}
+   }
+})
+  client.connect();
+  server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+  });
